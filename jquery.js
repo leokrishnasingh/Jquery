7cc846b134cac84c5baa425f870a9b6869f652c6
leokrishnasingh <leokrishna.singh@gmail.com>
2023-07-03 00:34:01 +0530

    Initial commit

---
+++ jquery.js
@@ -0,0 +1,13 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+    $('p').click(function () {
+        console.log("clicked");
+        }
+    );
+
+});
+
+
